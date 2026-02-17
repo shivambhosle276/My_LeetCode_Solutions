0fe61e9fa9f2e57c0af612f27bbaf2f663ad6260
shivambhosle276 <shivambhosle276@gmail.com>
2026-02-17 11:13:57 +0530

    Time: 0 ms (100%), Space: 20.2 MB (97.88%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int j=0;int l=0;int r=0;
+        while(r<nums.size()-1)
+        {
+             int far=0;
+            for(int i=l;i<=r;i++)
+            {
+                far=max(i+nums[i],far);
+
+            }
+            l=r+1;
+            r=far;
+            j=j+1;
+        }
+        return j;
+        
+    }
+};
