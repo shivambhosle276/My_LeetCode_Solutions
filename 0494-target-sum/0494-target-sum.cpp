962a23f2f66d51c08fa9ae056f1eb75ede0c7a0e
shivambhosle276 <shivambhosle276@gmail.com>
2026-05-22 13:58:29 +0530

    Time: 927 ms (7.65%), Space: 11.5 MB (99.21%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int ind, vector<int>& nums, int target) {
+        if (ind == nums.size() )
+        {
+           if(target == 0)
+               return 1;
+           else
+               return 0;
+        }
+        int pos = nums[ind] + solve(ind + 1, nums, target - nums[ind]);
+        int neg = -nums[ind] + solve(ind + 1, nums, target + nums[ind]);
+        return pos+neg;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+          return solve(0, nums, target);
+    }
+};
