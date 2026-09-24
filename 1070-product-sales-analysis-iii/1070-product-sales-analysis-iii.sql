94cea69bbb4dd87aa3f680cbcc94db913e019a2f
shivambhosle276 <shivambhosle276@gmail.com>
2026-09-24 16:45:49 +0530

    Time: 785 ms (48.36%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,6 +1,10 @@
-select product_id, year as first_year, quantity, price 
-from sales where (product_id, year) in (
-    select product_id, min(year)
-    from sales
-    group by product_id
-)
+SELECT product_id,
+       year AS first_year,
+       quantity,
+       price
+FROM Sales
+WHERE (product_id, year) IN (
+    SELECT product_id, MIN(year)
+    FROM Sales
+    GROUP BY product_id
+);
